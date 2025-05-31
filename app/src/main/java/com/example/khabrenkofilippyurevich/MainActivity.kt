b6dd58bd4410e48043d2f5a7f7b52d52e9ea28e8
licht225 <azzy1337r@gmail.com>
2025-05-31 14:39:14 +0300

    CHANGE COLOR

--- app/src/main/java/com/example/khabrenkofilippyurevich/MainActivity.kt
+++ app/src/main/java/com/example/khabrenkofilippyurevich/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.khabrenkofilippyurevich
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
+    private var currentColorIndex = 0
+
+    // Массив цветов в цикле
+    private val colors = arrayOf(
+        Color.parseColor("#FFA500"),
+        Color.parseColor("#8A2BE2"),
+        Color.parseColor("#808080")
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val colorSquare = findViewById<View>(R.id.colorSquare)
+
+        val switchButton = findViewById<Button>(R.id.switchColorButton)
+
+        colorSquare.setBackgroundColor(colors[currentColorIndex])
+
+        switchButton.setOnClickListener {
+            currentColorIndex = (currentColorIndex + 1) % colors.size
+            colorSquare.setBackgroundColor(colors[currentColorIndex])
+        }
     }
 }
